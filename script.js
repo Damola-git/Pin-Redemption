ff904138f0aa3569a52880bff58ac77d8a9131a5
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2025-02-04 17:48:50 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+/*fetch('pins.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error fetching pins.json:', error);
+    });*/
+const form = document.getElementById("pinForm");
+const userInput = document.getElementById("pin");
+const feedbackContainer = document.getElementById("feedbackContainer");
+
+    function verifyPin(userInput) {
+        fetch('pins.json')
+            .then(response => response.json())
+            .then(data => {
+                const pins = data.pins; // Assuming the JSON structure has a "pins" array
+                if (pins.includes(userInput.trim())) {
+                    feedbackContainer.innerText = 'Pin is valid';
+                } else {
+                    feedbackContainer.innerText = 'Pin is invalid';
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching pins.json:', error);
+            });
+
+            
+    }
+    form.addEventListener("submit", (event) => {
+        event.preventDefault(); 
+        verifyPin(userInput.value);
+      });
+      
+   
